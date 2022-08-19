bea4089af09d183434cfd97a13aac5b9ec932a2a
pooja <poojaramesh076@gmail.com>
2022-08-19 13:57:28 +0530

    myapp-1:added compnents and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'analytics',component:AnalyticsComponent},
+  {path:'products',component:ProductsComponent},
+  {path:'settings',component:SettingsComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'about',component:AboutComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  //{ path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,24 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { ProductsComponent } from './products/products.component';
+import { SettingsComponent } from './settings/settings.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SideNavComponent
+    SideNavComponent,
+    HomeComponent,
+    AnalyticsComponent,
+    ProductsComponent,
+    SettingsComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
